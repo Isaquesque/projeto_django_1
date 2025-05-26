088070aa4196450c5338ebd7f24f9f7ef07d486d
isaque <isaquesantosdequeirozq@gmail.com>
2025-05-26 16:56:03 -0300

    criando dados fake para a injeção de dados nos cards de receita

--- recipes/views.py
+++ recipes/views.py
@@ -1,8 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from utils.fake_data_generator import DataGenerator
 
 def home(request):
-    return render(request, "home.html")
+    data_list = []
+    for _ in range(6):
+        data_list.append(DataGenerator.generate())
+
+    return render(request, "home.html", context={"data_list":data_list})
 
 def recipe_card(request, id):
-    return render(request, "recipes_view.html")
+    data = DataGenerator.generate()
+
+    return render(request, "recipes_view.html", context={"data":data})
