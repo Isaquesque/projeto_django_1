a40a080f0de3044357933764f2e8cc686f8517d7
isaque <isaquesantosdequeirozq@gmail.com>
2025-05-27 21:45:38 -0300

    adicionando lógica nas views

--- recipes/views.py
+++ recipes/views.py
@@ -1,15 +1,28 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from utils.fake_data_generator import DataGenerator
+from .models import Recipe, Category
 
 def home(request):
-    data_list = []
-    for _ in range(6):
-        data_list.append(DataGenerator.generate())
+    recipes = list(Recipe.objects.all().filter(is_published = True))
 
-    return render(request, "home.html", context={"data_list":data_list})
+    return render(request, "home.html", context={"recipes":recipes})
 
-def recipe_card(request, id):
-    data = DataGenerator.generate()
+def recipe_details(request, recipe_id):
+    recipe = Recipe.objects.filter(id=recipe_id, is_published = True)
 
-    return render(request, "recipes_view.html", context={"data":data})
+    if(recipe):
+        recipe = recipe[0]
+
+    return render(request, "recipes_view.html", context={"recipe":recipe})
+
+def category_recipes(request, category_id):
+    recipes = list(Recipe.objects.filter(category__id = category_id, is_published = True))
+    category_name = Category.objects.filter(id=category_id)
+
+    if(category_name):
+        category_name = category_name[0].name
+    else:
+        category_name = ""
+
+    return render(request, "category_view.html", context={"recipes":recipes, "category_name":category_name})
