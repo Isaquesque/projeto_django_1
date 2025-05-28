4239bc02bf30d9a4b8c393a09f061e99bd2420e0
isaque <isaquesantosdequeirozq@gmail.com>
2025-05-27 21:44:27 -0300

    modificando rotas existentes e adicionando nova rota de receitas por categoria

--- recipes/urls.py
+++ recipes/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home),
-    path('recipes/<int:id>/', views.recipe_card),
+    path('', views.home, name="home"),
+    path('recipe/<int:recipe_id>/', views.recipe_details, name="recipe"),
+    path('recipes/category/<int:category_id>/', views.category_recipes, name="category"),
 ]
