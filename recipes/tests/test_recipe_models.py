de3f5edecb1826eb44407ccefc87d66a9639020e
isaque <isaquesantosdequeirozq@gmail.com>
2025-05-30 18:39:07 -0300

    criando testes parametrizados para verificar a validação de número de caracteres do model recipe

--- recipes/tests/test_recipe_models.py
+++ recipes/tests/test_recipe_models.py
@@ -1,14 +1,22 @@
 from .test_recipe_base_data import TestRecipeBaseData
 from recipes.models import User, Category, Recipe
 from django.core.exceptions import ValidationError
-import pytest
+from parameterized import parameterized
 
 class TestModelRecipe(TestRecipeBaseData):
     def setUp(self):
         self.recipe = self.make_recipe()
-
+        
+    @parameterized.expand(
+            [
+                ("title", 65),
+                ("description", 165),
+                ("preparation_time_unit", 65),
+                ("servings_unit", 65)
+            ]
+    )
     def test_model_recipe_raises_error_if_size_of_title_is_greater_than_65_chars(self, field, length):
-        self.recipe.title = "a" * 70
+        setattr(self.recipe, field, "a" * (length + 1))
         was_validate_correctly = False
         try:
             self.recipe.clean_fields()
