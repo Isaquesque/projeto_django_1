4e817f6644043540f6cad73d215db894ce87acee
isaque <isaquesantosdequeirozq@gmail.com>
2025-06-02 19:19:49 -0300

    criando a rota de pesquisa de receitas

--- recipes/urls.py
+++ recipes/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from . import views
+from django.http import HttpResponse
 
 urlpatterns = [
     path('', views.home, name="home"),
     path('recipe/<int:recipe_id>/', views.recipe_details, name="recipe"),
     path('recipes/category/<int:category_id>/', views.category_recipes, name="category"),
+    path('recipes/search/', views.recipes_search, name="search")
 ]
