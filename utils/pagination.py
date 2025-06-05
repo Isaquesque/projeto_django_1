eb979157ef190fcb3415396bd0c603f78bf688c0
isaque <isaquesantosdequeirozq@gmail.com>
2025-06-04 22:23:05 -0300

    algoritmo que retorna uma lista com os números das páginas a serem exibidas na paginação

--- utils/pagination.py
+++ utils/pagination.py
@@ -1,33 +1,43 @@
-
 def pagination(
-        objects, 
-        num_pages: int, 
-        num_item_per_page: int, 
+        num_total_pages: int,
+        num_pages: int,  
         current_page: int,
         num_pages_before_current_page:int,
         num_pages_after_current_page: int
     ):
+
     if num_pages_before_current_page + num_pages_after_current_page != (num_pages - 1):
         return
     
-    num_total_pages = len(objects) // num_item_per_page if len(objects) % num_item_per_page == 0 else (len(objects) // num_item_per_page) + 1
-    
     first_page = 1
     last_page = num_total_pages
+    pagination_list = []
 
-    if current_page - num_pages_before_current_page <= first_page:
-        return list(range(first_page, num_pages + 1))
+    if num_pages > num_total_pages:
+        pagination_list =  list(range(1, num_total_pages+1))
+    elif current_page - num_pages_before_current_page <= first_page:
+        pagination_list = list(range(first_page, num_pages + 1))
     elif current_page + num_pages_after_current_page > last_page:
-        return list(range(last_page - num_pages + 1, last_page + 1))
+        pagination_list = list(range(num_total_pages - num_pages + 1, num_total_pages + 1))
     else:
-        return list(range(current_page - num_pages_before_current_page, current_page + num_pages_after_current_page + 1))
+        pagination_list = list(range(current_page - num_pages_before_current_page, current_page + num_pages_after_current_page + 1))
 
-print(pagination(
-    range(1,101),
-    5,
-    6,
-    10,
-    2,
-    2
+    return {
+        "pagination_list": pagination_list,
+        "current_page":current_page,
+        "first_page":first_page,
+        "last_page":last_page,
+        "first_page_is_in_range": first_page in pagination_list,
+        "last_page_is_in_range": last_page in pagination_list,
+        "has_more_than_one_page": len(pagination_list) > 1
+    }
+
+if __name__ == "__main__":
+    print(pagination(
+        100,
+        4,
+        19,
+        1,
+        2
+        )
     )
-)
