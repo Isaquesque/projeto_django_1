cb7fffa59ea566f910a8a7327096c705c767005c
isaque <isaquesantosdequeirozq@gmail.com>
2025-06-06 18:26:32 -0300

    melhorando o algoritmo de páginação

--- utils/pagination.py
+++ utils/pagination.py
@@ -4,23 +4,37 @@ def pagination(
         current_page: int,
         num_pages_before_current_page:int,
         num_pages_after_current_page: int
-    ):
+    ) -> dict:
 
     if num_pages_before_current_page + num_pages_after_current_page != (num_pages - 1):
-        return
+        msg_error = "a soma da quantidade de páginas antes e depois da página atual \
+         deve dar a quantidade de páginas a serem agrupadas menos um"
+        raise ValueError(msg_error)
+    
+    if current_page < 1 or current_page > num_total_pages:
+        msg_error = f"a página atual deve estar entre {1} e {num_total_pages}"
+        raise ValueError(msg_error)
     
     first_page = 1
     last_page = num_total_pages
     pagination_list = []
 
+    start_of_range = current_page - num_pages_before_current_page
+    end_of_range = current_page + num_pages_after_current_page + 1
+    middle_list = list(range(start_of_range, end_of_range))
+
+    initial_list = list(range(first_page, num_pages + 1))
+    final_list = list(range(last_page - num_pages + 1, last_page + 1))
+    total_list = list(range(first_page, last_page + 1))
+
     if num_pages > num_total_pages:
-        pagination_list =  list(range(1, num_total_pages+1))
-    elif current_page - num_pages_before_current_page <= first_page:
-        pagination_list = list(range(first_page, num_pages + 1))
+        pagination_list =  total_list
+    elif current_page - num_pages_before_current_page < first_page:
+        pagination_list = initial_list
     elif current_page + num_pages_after_current_page > last_page:
-        pagination_list = list(range(num_total_pages - num_pages + 1, num_total_pages + 1))
+        pagination_list = final_list
     else:
-        pagination_list = list(range(current_page - num_pages_before_current_page, current_page + num_pages_after_current_page + 1))
+        pagination_list = middle_list
 
     return {
         "pagination_list": pagination_list,
