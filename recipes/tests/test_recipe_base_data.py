702b4317f448870be6b04a772b8faa8d978fd702
isaque <isaquesantosdequeirozq@gmail.com>
2025-06-06 18:19:38 -0300

    criando a função make_recipes que permite a criação de várias receitas ao mesmo tempo

--- recipes/tests/test_recipe_base_data.py
+++ recipes/tests/test_recipe_base_data.py
@@ -1,5 +1,6 @@
 from django.test import TestCase
 from recipes.models import User,Category,Recipe
+from utils.fake_data_generator import DataGenerator
 
 class TestRecipeBaseData(TestCase):
     def make_author(
@@ -49,8 +50,40 @@ class TestRecipeBaseData(TestCase):
             "preparation_steps": preparation_steps,
             "preparation_steps_is_html": preparation_steps_is_html,
             "is_published": is_published,
-            "category": self.make_category(**category),
-            "author": self.make_author(**author)
+            "category": category if isinstance(category, Category) else self.make_category(**category),
+            "author": author if isinstance(author, User) else self.make_author(**author)
         }
 
         return Recipe.objects.create(**recipe_data)
+    
+    def make_recipes(
+        self, 
+        category_info: list[dict]
+    ):
+        """
+            category_info - ex: [{
+                "category": "name" | category
+                "author" : "user" | User
+                "qty_recipes": 5
+                "published": True
+            }]
+
+            Você pode fornecer um nome em category para criar uma categoria com este nome,
+            ou então pode fornecer diretamente uma instância de Category em category.
+
+            Para author é da mesma maneira.
+
+        """
+        for info in category_info:
+            category = info["category"] if isinstance(info["category"],Category) else self.make_category(info["category"])
+            author =   info["author"] if isinstance(info["author"],User) else self.make_author(info["author"])
+            for i in range(info["qty_recipes"]):
+                data = DataGenerator.generate()
+                self.make_recipe(
+                    title = data["title"],
+                    slug = data["slug"] + f"-{i+1}",
+                    is_published= info["published"],
+                    author= author,
+                    category= category
+                )
+
