99beda9b4bc2b8ce85bdeed00d19a44ba6304250
isaque <isaquesantosdequeirozq@gmail.com>
2025-06-12 16:11:09 -0300

    adicionando rota para a view register_create para tratamento de dados do form de registro

--- authors/urls.py
+++ authors/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("register/", views.register, name="register")
+    path("register/", views.register, name="register"),
+    path("register/create", views.register_create, name="register_create")
 ]
