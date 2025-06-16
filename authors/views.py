f0c6d154aa3240d8a0800101145ff3452a52f274
isaque <isaquesantosdequeirozq@gmail.com>
2025-06-15 23:22:53 -0300

    refatorando a view register para assumir todo o processo de criação de usuário e excluindo a view register_create que era uma view auxiliar

--- authors/views.py
+++ authors/views.py
@@ -1,22 +1,23 @@
 from django.shortcuts import render, redirect
 from .forms import RegisterForm
 from django.http import HttpResponse, Http404
+from django.contrib import messages
+from django.contrib.auth.models import User
 
 # Create your views here.
 def register(request):
     if request.method == "POST":
-        form = RegisterForm(request.POST)
-        if form.is_valid():
-            return HttpResponse("ok", status=200)
-    else: 
-        form = RegisterForm(request.session["POST"])
+        data = request.POST
+        bound_form = RegisterForm(data)
 
+        if bound_form.is_valid():
+            cleaned_data = bound_form.cleaned_data
+            del cleaned_data["password2"]
+            user = User.objects.create_user(**cleaned_data)
+            messages.add_message(request, messages.SUCCESS, f"Usuário {user.username} cadastrado com sucesso")
+        else:
+            messages.add_message(request, messages.ERROR, "O formulário possui erros. Corrija-os")
+            return render(request, "authors/register.html", context={"form":bound_form})
+
+    form = RegisterForm()
     return render(request, "authors/register.html", context={"form":form})
-
-def register_create(request):
-    if request.method == "GET":
-        raise Http404
-    
-    POST = request.POST
-    request.session["POST"] = POST
-    return redirect("register")
